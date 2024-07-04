4f653cbf33c82a5833cc349319abc8b98939d3bc
Maruf-Raju-pro <Muhammedmarufraju@gmail.com>
2024-07-04 11:13:54 +0600

    working on manage services

--- src/Pages/Private/ManageServices/ManageServices.jsx
+++ src/Pages/Private/ManageServices/ManageServices.jsx
@@ -1,11 +1,52 @@
+import { useQuery } from '@tanstack/react-query';
+import UseAxiosSecure from '../../../Hooks/AxiosBase/UseAxiosSecure';
+import Loading from '../../Loadng/Loading';
+import ManageServicesCard from './ManageServicesCard';
+import { useContext } from 'react';
+import { AuthContext } from '../../../Provider/CredProvider';
+
 const ManageServices = () => {
+  const { user } = useContext(AuthContext);
+  const axiosSecure = UseAxiosSecure();
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['services-data'],
+    queryFn: async () => {
+      const { data } = await axiosSecure.get(`/services?email=${user?.email}`);
+      return data;
+    },
+  });
+  if (isLoading) <Loading />;
+ console.log(data);
   return (
-    <div className="flex items-center justify-center bg-[#031926] min-h-96 my-10">
-      <h2 className="p-5 text-5xl font-bold capitalize  text-center bg-[#9381ff] rounded-lg shadow-xl">
-        {' '}
-        ManageServices loading please wait{' '}
-      </h2>
-    </div>
+    <>
+      <div>
+        <h2 className="text-3xl font-bold text-center   p-10 rounded-lg ">
+          Manage your services
+        </h2>
+      </div>
+      <div>
+        <div className="overflow-x-auto my-5 border bg-blue-100">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>Service info</th>
+                <th>Provider</th>
+                <th>Total Bookings</th>
+                <th>Action</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody className="">
+              {data?.map((item, index) => {
+                return <ManageServicesCard key={index} item={item} />;
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
   );
 };
 
---
+++ src/Pages/Private/ManageServices/ManageServicesCard.jsx
@@ -0,0 +1,58 @@
+import PropTypes from 'prop-types';
+const ManageServicesCard = ({ item }) => {
+  const {
+    imageURL,
+    serviceName,
+    price,
+    providerEmail,
+    providerImage,
+    providerName,
+  } = item;
+
+  return (
+    <tr>
+      <td>
+        <div className="flex items-center  gap-2">
+          <div className="w-16 mask mask-hexagon">
+            <img src={imageURL} />
+          </div>
+          <div>
+            {/* name  */}
+            <p>{serviceName} </p>
+            {/* price  */}
+            <p>{price}$</p>
+          </div>
+        </div>
+      </td>
+
+      <td>
+        <div className="flex items-center gap-3">
+          <div className="avatar">
+            <div className="mask mask-squircle h-12 w-12">
+              <img src={providerImage} alt="Avatar Tailwind CSS Component" />
+            </div>
+          </div>
+          <div>
+            <div className="font-bold"> {providerName} </div>
+            <div className="text-sm opacity-50">{providerEmail}</div>
+          </div>
+        </div>
+      </td>
+
+      <td>
+        <span className="p-5 ">20</span>
+      </td>
+      <td>
+        <button className="btn"> Update </button>
+      </td>
+
+      <th>
+        <button className="btn"> Delete </button>
+      </th>
+    </tr>
+  );
+};
+ManageServicesCard.propTypes = {
+  item: PropTypes.object.isRequired,
+};
+export default ManageServicesCard;
